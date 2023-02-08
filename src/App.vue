84e0fa0791d16b4dbd60f7304c30a8a2b9dd85c4
JustRollingDevs <justrollingdevs@protonmail.com>
2023-02-08 10:31:28 +0200

    added fade transition to every route.path

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <script setup lang="ts">
-  import { RouterView, useRoute } from 'vue-router';
-  // components
-  import Navbar from './components/Navbar.vue';
-  import Footer from './components/Footer.vue';
-  import SelectedPrints from './components/SelectedPrints.vue';
+import { RouterView, useRoute } from 'vue-router';
+// components
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+import SelectedPrints from './components/SelectedPrints.vue';
 
-  const route = useRoute();
+const route = useRoute();
 </script>
 
 <template>
   <header>
     <Navbar />
   </header>
-  <RouterView />
+  <Transition name="fade">
+    <RouterView :key="route.path" />
+  </Transition>
   <KeepAlive>
     <SelectedPrints dir="rtl" v-if="route.name != 'printing'" />
   </KeepAlive>
   <Footer />
 </template>
+
+<style lang="scss" scoped>
+@import './assets/mixins.scss';
+@include fade-transition;
+</style>

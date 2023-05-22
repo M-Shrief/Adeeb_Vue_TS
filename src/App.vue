749de0cb86bfcc745d5c0e6f994539cfac1749fc
JustRollingDevs <m.shrief.dev@proton.me>
2023-05-22 11:56:24 +0300

    extracted footer component to App.vue because we don't need it as a component, made HttpPopUp an async component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import { defineAsyncComponent } from 'vue'
 // components
 import Navbar from './components/Navbar.vue';
-import Footer from './components/Footer.vue';
-import HttpPopUp from './components/NotificationsCenter/HttpPopUp.vue';
+// import Footer from './components/Footer.vue';
+const HttpPopUp = defineAsyncComponent(() => import('./components/NotificationsCenter/HttpPopUp.vue'))
 </script>
 
 <template>
@@ -15,10 +16,32 @@ import HttpPopUp from './components/NotificationsCenter/HttpPopUp.vue';
       <component :is="Component" />
     </transition>
   </router-view>
-  <Footer />
+  <footer>
+    <p>
+      Copyright &copy; 2022 |
+      <a href="#" title="Github profile" class="profile">M. Shrief</a>
+    </p>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
 @import './assets/mixins.scss';
 @include fade-transition;
+
+footer {
+  text-align: center;
+  margin-top: 1.6rem;
+  font-size: 1.2rem;
+  font-weight: 700;
+
+  .profile {
+    color: #004e64;
+    text-decoration: none;
+  }
+
+  @include mQ($breakpoint-sm) {
+    margin-top: 1rem;
+    font-size: 1rem;
+  }
+}
 </style>

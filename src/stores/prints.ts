4b750b5718b2be8c253f090d2ae4766294db3d09
M-Shrief <m.shrief.dev@proton.me>
2023-07-09 22:43:37 +0300

    refactored to use the composition API instead with vueuse/core-->useSessionStorage() instead of pinia plugin

--- src/stores/prints.ts
+++ src/stores/prints.ts
@@ -1,38 +1,37 @@
+import {ref, computed} from 'vue';
 import { defineStore } from 'pinia';
+// Types
 import type { Print } from './__types__';
+// Composables
+import {useSessionStorage} from '@vueuse/core';
 
-export const usePrintsStore = defineStore('prints', {
-  state: () => {
-    return {
-      prints: [] as Print[],
-    };
-  },
-  getters: {
-    getPrints(state): Print[] {
-      return state.prints;
-    },
-  },
-  actions: {
-    addPrint(print: Print) {
-      const printsIds = this.prints.map(
-        (printItem) =>
-          // printItem._id ? printItem._id : printItem.verses[0]._id
-          printItem._id
-      );
-      // const printId = print._id ? print._id : print.verses[0]._id;
-      if (!printsIds.includes(print._id)) {
-        this.prints.push(print);
-      }
-    },
-    prepPrints(prints: Print[]) {
-      // it's normal in big orders to repeat the same print
-      this.prints = this.prints.concat(prints);
-    },
-    removePrint(print: Print) {
-      let printIndex = this.getPrints
-        .map((verse) => verse._id)
-        .indexOf(print._id);
-      this.getPrints.splice(printIndex, 1);
-    },
-  },
+
+export const usePrintsStore = defineStore('prints', () => {
+  const prints =  ref(useSessionStorage('prints', [] as Print[]));
+  const getPrints =  computed<Print[]>(() => {
+    return prints.value;
+  });
+  function addPrint(print: Print) {
+    const printsIds = prints.value.map(
+      (printItem) =>
+        // printItem._id ? printItem._id : printItem.verses[0]._id
+        printItem._id
+    );
+    // const printId = print._id ? print._id : print.verses[0]._id;
+    if (!printsIds.includes(print._id)) {
+      prints.value.push(print);
+    }
+  };
+  function prepPrints(prints: Print[]) {
+    // it's normal in big orders to repeat the same print
+    prints.value = prints.value.concat(prints);
+  };
+  function removePrint(print: Print) {
+    let printIndex = prints.value
+      .map((verse) => verse._id)
+      .indexOf(print._id);
+    prints.value.splice(printIndex, 1);
+  };
+
+  return {getPrints, addPrint, prepPrints, removePrint}
 });

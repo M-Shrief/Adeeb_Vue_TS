cb04656b043c9bbcfea0252f1af1ca4f1b295ba6
M-Shrief <m.shrief.dev@proton.me>
2023-07-24 23:58:23 +0300

    created 2 instances from axios for reuse.

---
+++ src/shared/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const baseHttp = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: false,
+})
+
+export const withAuthHttp = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+    headers: {
+        common: {
+            Authorization: `Bearer ${sessionStorage.getItem("accessToken")}`
+        }
+    }
+})
+
+
+const partnerHttp = axios.create({})
